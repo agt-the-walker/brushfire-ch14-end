b1c2ecf9e479efa4a202064e2f63b3208bbd3fdd
Jerome Zago <jerome.zago@gmail.com>
2017-06-11 11:09:42 +0200

    Refactoring a test using fixtures and helper functions

--- test/integration/create-user.js
+++ test/integration/create-user.js
@@ -1,44 +1,14 @@
 var assert = require('assert');
 var request = require('supertest');
 
-var Passwords = require('machinepack-passwords');
-
 describe('User Controller :: ', function() {
   describe('POST /user/signup :: ', function() {
     describe('When logged in :: ', function() {
       var agent;
       before(function(done) {
+        var createTestUserAndAuthenticate = require('../utils/create-logged-in-user');
         agent = request.agent(sails.hooks.http.app);
-
-        Passwords.encryptPassword({
-          password: 'abc123'
-        })
-        .exec({
-          error: done,
-          success: function(password) {
-            User.create({
-              username: 'testtest',
-              email: 'test@test.com',
-              encryptedPassword: password
-            })
-            .exec(function(err, user) {
-              if(err) { return done(err); }
-
-              agent
-              .put('/login')
-              .send({
-                username: 'testtest',
-                password: 'abc123'
-              })
-              .set('Content-Type', 'application/json')
-              .end(function(err, res) {
-                if(err) { return done(err); }
-                console.log('res.status', res.status);
-                return done();
-              });
-            });
-          }
-        });
+        createTestUserAndAuthenticate(agent, done);
       });
 
       it('should return a 403 response code', function(done) {
